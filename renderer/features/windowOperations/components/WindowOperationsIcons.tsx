49bb70873d030bb964f635ef02510810fafeab23
Timofei Kurohtin <forgwent@gmail.com>
2022-12-14 23:53:23 +0300

    Transitions

--- renderer/features/windowOperations/components/WindowOperationsIcons.tsx
+++ renderer/features/windowOperations/components/WindowOperationsIcons.tsx
@@ -9,7 +9,7 @@ const WindowOperationsIcons = () => {
       <div className="non-draggable flex items-center">
         <div
           onClick={() => setOpen(true)}
-          className="mr-1 flex h-min w-10 cursor-pointer items-center justify-center rounded p-1 outline-1 hover:bg-gray-300 active:scale-105 active:outline active:outline-gray-400"
+          className="mr-1 flex h-min w-10 cursor-pointer items-center justify-center rounded p-1 outline-1 transition-all hover:bg-gray-300 active:scale-105 active:outline active:outline-gray-400"
         >
           <Image
             src="/menubar/settings_FILL0_wght400_GRAD0_opsz48.svg"
@@ -20,7 +20,7 @@ const WindowOperationsIcons = () => {
         </div>
         <div
           onClick={() => ipcRenderer.invoke("min-window")}
-          className="flex h-min w-10 cursor-pointer items-center justify-center rounded-l p-1 outline-1 hover:bg-gray-300 active:scale-105 active:outline active:outline-emerald-400"
+          className="flex h-min w-10 cursor-pointer items-center justify-center rounded-l p-1 outline-1 transition-all hover:bg-gray-300 active:scale-105 active:outline active:outline-emerald-400"
         >
           <Image
             src="/menubar/minimize_FILL0_wght400_GRAD0_opsz48.svg"
@@ -31,7 +31,7 @@ const WindowOperationsIcons = () => {
         </div>
         <div
           onClick={() => ipcRenderer.invoke("max-window")}
-          className="flex h-min w-10 cursor-pointer items-center justify-center p-1 outline-1 hover:bg-gray-300 active:scale-105 active:outline active:outline-yellow-400"
+          className="flex h-min w-10 cursor-pointer items-center justify-center p-1 outline-1 transition-all hover:bg-gray-300 active:scale-105 active:outline active:outline-yellow-400"
         >
           <Image
             src="/menubar/open_in_full_FILL0_wght400_GRAD0_opsz48.svg"
@@ -42,7 +42,7 @@ const WindowOperationsIcons = () => {
         </div>
         <div
           onClick={() => ipcRenderer.invoke("quit-app")}
-          className="flex h-min w-10 cursor-pointer items-center justify-center rounded-r p-1 outline-1 hover:bg-gray-300 active:scale-105 active:outline active:outline-red-400"
+          className="flex h-min w-10 cursor-pointer items-center justify-center rounded-r p-1 outline-1 transition-all hover:bg-gray-300 active:scale-105 active:outline active:outline-red-400"
         >
           <Image
             src="/editor/close_FILL0_wght400_GRAD0_opsz48.svg"
